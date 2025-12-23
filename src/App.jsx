36d98bf4a4163a3e2d9bfe91ab5003b8bc24ce3e
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-12-23 16:25:35 +0530

    Implement Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Home from "./views/Home.jsx";
+import Login from "./views/Login.jsx";
+import SignUp from "./views/SignUp.jsx";
+import Profile from "./views/Profile.jsx";
+
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-  return <div className="">Authentication</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
